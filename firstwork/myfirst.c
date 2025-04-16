0455da30f11421a21001fba2175518747c00950d
MELIMAJOEL <lilmemadaplug@gmail.com>
2025-04-16 05:44:38 +0100

    Add files via upload

---
+++ firstwork/myfirst.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+     printf("hello world\n");
+      printf("have good day\n");
+     int x = 15;
+     int y = 12;
+     int sum = x * y;
+     printf("%d",sum);
+
+     return 0;
+}
